d0d9f6b7f15f9df7b5cfa1b24f9fafa436a9df11
Rory Quinlan <rjamesquinlan@gmail.com>
2016-12-09 23:35:07 +0700

    redirect to success page after subscribing

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -19,5 +19,6 @@ from weather_app import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^success', views.subscription_success, name='subscription_success'),
     url(r'^$', views.user_new, name='user_new'),
 ]
--- weather_app/views.py
+++ weather_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, render_to_response
 from .forms import UserForm
 
 # Create your views here.
@@ -7,8 +7,11 @@ def user_new(request):
         form = UserForm(request.POST)
         if form.is_valid():
             user = form.save(commit=True)
-            return redirect('/')
+            return redirect('/success')
     else:
         form = UserForm()
 
     return render(request, 'user_create.html', {'form': form})
+
+def subscription_success(request):
+   return render_to_response('subscription_success.html')
